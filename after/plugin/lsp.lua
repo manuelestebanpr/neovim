c1df0865e4e0f30d33a1e37eca020d44ae83c82e
Manolo <manuelestebanpr@gmail.com>
2025-10-31 20:07:18 -0500

    Removing jdlst as not currently usgin any java project

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -54,10 +54,3 @@ vim.diagnostic.config({
     },
 })
 
-
-vim.api.nvim_create_autocmd('FileType', {
-    pattern = 'java',
-    callback = function(args)
-        require'jdtls.jdtls_setup'.setup()
-    end
-})
--- lua/config/set.lua
+++ lua/config/set.lua
@@ -1,9 +1,10 @@
 vim.opt.relativenumber = false
 vim.opt.smartindent = true
 vim.opt.nu = true
-vim.opt.winborder = 'rounded' 
+vim.opt.winborder = 'rounded'
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
-
+vim.opt.termguicolors = true
+vim.opt.clipboard = 'unnamedplus'
