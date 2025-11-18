09cbdc4532183b616c62b4525df83c8d181dfdb3
Manuel Perez <>
2025-11-18 13:12:02 -0500

    Adding remap and set values

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -1,9 +1,9 @@
 vim.opt.relativenumber = false
 vim.opt.smartindent = true
 vim.opt.nu = true
-vim.opt.winborder = 'rounded' 
+vim.opt.winborder = 'rounded'
 vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 
